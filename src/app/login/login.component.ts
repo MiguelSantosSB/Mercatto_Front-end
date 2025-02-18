1c4c2237d80b197351e8ef245f8f5524b491d2a2
Miguel Santos <miguel.santoss085@gmail.com>
2025-02-18 15:44:07 -0300

    tela de login concluida

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin(): void {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response: any) => {
+        this.router.navigate(['/']);
+      },
+      error: (error: any) => {
+        console.error('Erro no login:', error);
+      }
+    });
+  }
+
+  onCadastro(): void {
+    // TODO: Implementar navegação para tela de cadastro
+  }
+}
