116919c645a20c3b011c5c1a41f8e460aa20e251
Miguel Santos <miguel.santoss085@gmail.com>
2025-02-19 00:26:22 -0300

    Espaço do Vendedor

---
+++ src/app/owner-space/owner-space.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owner-space',
+  standalone: true,
+  imports: [],
+  templateUrl: './owner-space.component.html',
+  styleUrl: './owner-space.component.scss'
+})
+export class OwnerSpaceComponent {
+
+}
